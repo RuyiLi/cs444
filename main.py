b12bcc8a354267bd47c27fd8bc16e74bc1d70974
WillFlame <flameyanai@gmail.com>
2024-01-24 21:20:12 -0500

    Add remaining literals

--- main.py
+++ main.py
@@ -2,35 +2,38 @@ from lark import Lark
 
 l = Lark(
     """
-            ?literal: integer_l
-            | boolean_l
-            # | char_l
-            # | string_l
-            # | null_l
+            ?literal:  integer_l
+                     | boolean_l
+                     | char_l
+                     | string_l
+                     | null_l
          
-            integer_l: /[0-9]+/
+            integer_l: INT
 
-            boolean_l: "true" -> true
-            | "false"         -> false
+            boolean_l: "true"    -> true
+                     | "false"   -> false
 
-            # char_l: "'" (LETTER+ | escape_seq) "'"
+            char_l: "'" (LETTER | escape_seq) "'"
+            escape_seq: "\\\\" (/[btnfr\\\"\\\'\\\\]/ | INT)
          
-            # escape_seq: "\\" (/[btnfr]/ | "\"" | "'" | "\\")
+            string_l: "\\\"" (LETTER | escape_seq)* "\\\""
+         
+            null_l: "null"
 
-            op: "+"  -> plus
-            | "-"    -> minus
-            | "*"    -> times
-            | "/"    -> divide
+            op:  "+"    -> plus
+               | "-"    -> minus
+               | "*"    -> times
+               | "/"    -> divide
 
-            ?expr: literal
-            | literal op expr
+            ?expr:  literal
+                  | literal op expr
 
-            %import common.NUMBER
+            %import common.INT
             %import common.LETTER
             %ignore " "           // Disregard spaces in text
          """,
     start="expr",
-    parser="lalr",
+    parser="lalr"
 )
 
-print(l.parse("true + 3").pretty())
+print(l.parse("'f' + 3 - '\\b' / '\\0177' + \"foo\\b\" + null").pretty())
