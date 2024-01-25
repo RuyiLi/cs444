ea79d2307da6886162721c5d0b6509eae2d0a3ca
Chinemerem Chigbo <cchigbo@uwaterloo.ca>
2024-01-24 22:55:44 -0500

    Parse test files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import glob
+import os
+
 from lark import Lark
 
 grammar = ""
@@ -10,4 +12,16 @@ for file in files:
 
 l = Lark(grammar, start="expr", parser="lalr")
 
-print(l.parse("'f' + 3 - '\\b' / '\\0177' + \"foo\\b\" >>>= null + abstract").pretty())
+print(l.parse("'f' + 3 - '\\b' / '\\0177' + \"foo\\b\" + null").pretty())
+
+test_directory = os.path.join(os.getcwd(), "assignment_testcases/a1")
+test_files = os.listdir(test_directory)
+for test_file in test_files:
+    with open(os.path.join(test_directory, test_file), "r") as f:
+        test_file_contents = f.read()
+        try:
+            # print(l.parse(test_file_contents).pretty())
+            pass
+        except:
+            # print(f"Failed {test_file}")
+            pass
