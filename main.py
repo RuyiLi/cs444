c39879cba57b5e55e90ae5eadcc5a4bbd51f194d
Ruyi Li <r397li@uwaterloo.ca>
2024-01-24 21:29:15 -0500

    load lark files

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
+import glob
 from lark import Lark
 
-l = Lark('''
-            ?literal: integer_l
-            | boolean_l
-            # | char_l
-            # | string_l
-            # | null_l
-         
-            integer_l: /[0-9]+/
+grammar = ""
 
-            boolean_l: "true" -> true
-            | "false"         -> false
+files = glob.glob(r"./grammar/*.lark")
+for file in files:
+    with open(file) as f:
+        grammar += "\n" + f.read()
 
-            # char_l: "'" (LETTER+ | escape_seq) "'"
-         
-            # escape_seq: "\\" (/[btnfr]/ | "\"" | "'" | "\\")
+lark = Lark(grammar, start="expr", parser="lalr")
 
-            op: "+"  -> plus
-            | "-"    -> minus
-            | "*"    -> times
-            | "/"    -> divide
-
-            ?expr: literal
-            | literal op expr
-
-            %import common.NUMBER
-            %import common.LETTER
-            %ignore " "           // Disregard spaces in text
-         ''', start="expr", parser="lalr")
-
-print(l.parse("true + 3").pretty())
+print(lark.parse("import foo;").pretty())
