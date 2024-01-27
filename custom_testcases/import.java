80d88e887507cdd4cda58f526eabacce9a97292a
Ruyi Li <r397li@uwaterloo.ca>
2024-01-27 15:58:46 -0500

    add import temp impl + tests

---
+++ custom_testcases/import.java
@@ -0,0 +1,6 @@
+import foo.bar.baz;
+import java.util.Vector;
+import javax.swing.*;
+import foo.* ;import _foo.$bar_4.* ;
+import something    .something1.    something2.   something3       ;
+                                    import single;
