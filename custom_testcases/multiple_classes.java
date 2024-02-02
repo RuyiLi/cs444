3932db7ec5e7238374ae89c5b2741c756b605232
WillFlame <flameyanai@gmail.com>
2024-02-02 14:55:52 -0500

    Prevent multiple types per file

---
+++ custom_testcases/multiple_classes.java
@@ -0,0 +1,9 @@
+public class multiple_classes {
+    protected int x;
+    public multiple_classes () {}
+}
+
+public class multiple_classes {
+    protected int y;
+    public multiple_classes () {}
+}
