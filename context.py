ff34720a66da2d10b3998d2a224480ab1d4e780f
WillFlame <flameyanai@gmail.com>
2024-02-14 19:16:15 -0500

    Fix indentation

--- context.py
+++ context.py
@@ -2,137 +2,146 @@ class SemanticError(Exception):
     pass
 
 class Context:
-	def __init__(self, parent, parent_node):
-		self.parent = parent
-		self.parent_node = parent_node
-		self.children = []
-		self.symbol_map = {}
+    def __init__(self, parent, parent_node):
+        self.parent = parent
+        self.parent_node = parent_node
+        self.children = []
+        self.symbol_map = {}
 
-	def declare(self, symbol):
-		if self.resolve(symbol.sym_id()) is not None:
-			raise SemanticError(f"Overlapping {symbol.node_type} in scope.")
+    def declare(self, symbol):
+        if self.resolve(symbol.sym_id()) is not None:
+            raise SemanticError(f"Overlapping {symbol.node_type} in scope.")
 
-		self.symbol_map[symbol.sym_id()] = symbol
+        self.symbol_map[symbol.sym_id()] = symbol
 
-	def resolve(self, id_hash):
-		if id_hash in self.symbol_map:
-			return self.symbol_map.get(id_hash)
+    def resolve(self, id_hash):
+        if id_hash in self.symbol_map:
+            return self.symbol_map.get(id_hash)
 
-		# Try looking in parent scope
-		if self.parent is not None:
-			return self.parent.resolve(id_hash)
+        # Try looking in parent scope
+        if self.parent is not None:
+            return self.parent.resolve(id_hash)
 
-		return None
+        return None
 
 class Symbol:
-	def __init__(self, context, name):
-		self.context = context
-		self.name = name
-		self.node_type = ""
+    def __init__(self, context, name):
+        self.context = context
+        self.name = name
+        self.node_type = ""
 
-	def sym_id(self):
-		return self.node_type + "^" + self.name
+    def sym_id(self):
+        return self.node_type + "^" + self.name
 
-	def hierarchy_check(self):
-		pass
+    def hierarchy_check(self):
+        pass
 
 class ClassDecl(Symbol):
-	def __init__(self, context, name, modifiers, extends, implements):
-		super().__init__(context, name)
-		self.node_type = "class_decl"
-		self.modifiers = modifiers
-		self.extends = extends
-		self.implements = implements
+    def __init__(self, context, name, modifiers, extends, implements):
+        super().__init__(context, name)
+        self.node_type = "class_decl"
+        self.modifiers = modifiers
+        self.extends = extends
+        self.implements = implements
 
-		self.fields = []
-		self.methods = []
+        self.fields = []
+        self.methods = []
 
-	def sym_id(self):
-		return "class_interface^" + self.name
+    def sym_id(self):
+        return "class_interface^" + self.name
 
-	def hierarchy_check(self):
-		for extend in self.extends:
-			exist_sym = self.context.resolve(f"class_interface^{extend}")
+    def hierarchy_check(self):
+        for extend in self.extends:
+            exist_sym = self.context.resolve(f"class_interface^{extend}")
 
-			if exist_sym is None:
-				raise SemanticError(f"Class {self.name} cannot extend class {extend} that does not exist.")
+            if exist_sym is None:
+                raise SemanticError(f"Class {self.name} cannot extend class {extend} that does not exist.")
 
-			if exist_sym.node_type == "interface_decl":
-				raise SemanticError(f"Class {self.name} cannot extend an interface ({extend}).")
+            if exist_sym.node_type == "interface_decl":
+                raise SemanticError(f"Class {self.name} cannot extend an interface ({extend}).")
 
-			if "final" in exist_sym.modifiers:
-				raise SemanticError(f"Class {self.name} cannot extend a final class ({extend}).")
+            if "final" in exist_sym.modifiers:
+                raise SemanticError(f"Class {self.name} cannot extend a final class ({extend}).")
+        
+        if len(set(self.extends)) < len(self.extends):
+            raise SemanticError(f"Duplicate class/interface in extends for class {self.name}")
 
-		for implement in self.implements:
-			exist_sym = self.context.resolve(f"class_interface^{implement}")
+        for implement in self.implements:
+            exist_sym = self.context.resolve(f"class_interface^{implement}")
 
-			if exist_sym is None:
-				raise SemanticError(f"Class {self.name} cannot extend class {implement} that does not exist.")
+            if exist_sym is None:
+                raise SemanticError(f"Class {self.name} cannot extend class {implement} that does not exist.")
 
-			if exist_sym.node_type == "class_decl":
-				raise SemanticError(f"Class {self.name} cannot implement a class ({implement}).")
+            if exist_sym.node_type == "class_decl":
+                raise SemanticError(f"Class {self.name} cannot implement a class ({implement}).")
+
+        if len(set(self.extends)) < len(self.extends):
+            raise SemanticError(f"Duplicate class/interface in implements for class {self.name}")
 
 class InterfaceDecl(Symbol):
-	def __init__(self, context, name, modifiers, extends):
-		super().__init__(context, name)
-		self.node_type = "interface_decl"
-		self.modifiers = modifiers
-		self.extends = extends
+    def __init__(self, context, name, modifiers, extends):
+        super().__init__(context, name)
+        self.node_type = "interface_decl"
+        self.modifiers = modifiers
+        self.extends = extends
 
-		self.fields = []
-		self.methods = []
+        self.fields = []
+        self.methods = []
 
-	def sym_id(self):
-		return "class_interface^" + self.name
+    def sym_id(self):
+        return "class_interface^" + self.name
 
-	def hierarchy_check(self):
-		for extend in self.extends:
-			exist_sym = self.context.resolve(f"class_interface^{extend}")
+    def hierarchy_check(self):
+        for extend in self.extends:
+            exist_sym = self.context.resolve(f"class_interface^{extend}")
 
-			if exist_sym is None:
-				raise SemanticError(f"Interface {self.name} cannot extend interface {extend} that does not exist.")
+            if exist_sym is None:
+                raise SemanticError(f"Interface {self.name} cannot extend interface {extend} that does not exist.")
 
-			if exist_sym.node_type == "class_decl":
-				raise SemanticError(f"Interface {self.name} cannot extend a class ({extend}).")
+            if exist_sym.node_type == "class_decl":
+                raise SemanticError(f"Interface {self.name} cannot extend a class ({extend}).")
+
+        if len(set(self.extends)) < len(self.extends):
+            raise SemanticError(f"Duplicate class/interface in extends for interface {self.name}")
 
 class FieldDecl(Symbol):
-	def __init__(self, context, name, modifiers, field_type):
-		super().__init__(context, name)
-		self.node_type = "field_decl"
-		self.modifiers = modifiers
-		self.sym_type = field_type
+    def __init__(self, context, name, modifiers, field_type):
+        super().__init__(context, name)
+        self.node_type = "field_decl"
+        self.modifiers = modifiers
+        self.sym_type = field_type
 
-		self.context.parent_node.fields.push(self)
+        self.context.parent_node.fields.push(self)
 
 class MethodDecl(Symbol):
-	def __init__(self, context, name, param_types, modifiers):
-		super().__init__(context, name)
-		self.node_type = "method_decl"
-		self.param_types = param_types
-		self.modifiers = modifiers
+    def __init__(self, context, name, param_types, modifiers):
+        super().__init__(context, name)
+        self.node_type = "method_decl"
+        self.param_types = param_types
+        self.modifiers = modifiers
 
-		self.context.parent_node.methods.push(self)
+        self.context.parent_node.methods.push(self)
 
-	def sym_id(self):
-		return self.name + "^" + ",".join(self.param_types) if self.param_types is not None else self.name
+    def sym_id(self):
+        return self.name + "^" + ",".join(self.param_types) if self.param_types is not None else self.name
 
 class LocalVarDecl(Symbol):
-	def __init__(self, context, name, var_type):
-		super().__init__(context, name)
-		self.node_type = "local_var_decl"
-		self.sym_type = var_type
+    def __init__(self, context, name, var_type):
+        super().__init__(context, name)
+        self.node_type = "local_var_decl"
+        self.sym_type = var_type
 
 class IfStmt(Symbol):
-	def __init__(self, context, name):
-		super().__init__(context, name)
+    def __init__(self, context, name):
+        super().__init__(context, name)
 
 
 class WhileStmt(Symbol):
-	def __init__(self, context, name):
-		super().__init__(context, name)
+    def __init__(self, context, name):
+        super().__init__(context, name)
 
 node_dict = {
-	"class_decl": ClassDecl,
-	"if_stmt": IfStmt,
-	"while_stmt": WhileStmt
+    "class_decl": ClassDecl,
+    "if_stmt": IfStmt,
+    "while_stmt": WhileStmt
 }
--- hierarchy_check.py
+++ hierarchy_check.py
@@ -1,10 +1,12 @@
 from context import Context, SemanticError
 
 def hierarchy_check(context: Context):
-	for sym_id, symbol in context.symbol_map.items():
-		# Check that the types of all symbols exist
-		symbol.hierarchy_check()
-		pass
+    for sym_id, symbol in context.symbol_map.items():
+        # Check that the types of all symbols exist
+        
+        # Check that every symbol meets its hierarchy criteria
+        symbol.hierarchy_check()
+        pass
 
-	for subcontext in context.children:
-		hierarchy_check(subcontext)
+    for subcontext in context.children:
+        hierarchy_check(subcontext)
