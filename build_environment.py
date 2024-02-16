94f7ab65bcf621e7cff7ffe43547f68d43d9682e
WillFlame <flameyanai@gmail.com>
2024-02-15 23:21:35 -0500

    Simplifications, some bug fixes

--- build_environment.py
+++ build_environment.py
@@ -28,14 +28,19 @@ def build_environment(tree: ParseTree, context: Context):
 def get_tree_first_child(tree: ParseTree, name: str):
     return next(tree.find_pred(lambda c: c.data == name)).children[0]
 
+def get_child_token(tree: ParseTree, name: str) -> str:
+    return next(filter(lambda c: isinstance(c, Token) and c.type == name, tree.children)).value
 
-def get_nested_token(tree: ParseTree, name: str):
+def get_nested_token(tree: ParseTree, name: str) -> str:
     return next(tree.scan_values(lambda v: isinstance(v, Token) and v.type == name)).value
-
 
 def get_tree_token(tree: ParseTree, tree_name: str, token_name: str):
     return get_nested_token(next(tree.find_pred(lambda c: c.data == tree_name)), token_name)
 
+def get_tree_child_token(tree: ParseTree, tree_name: str, token_name: str):
+    return get_child_token(
+        next(tree.find_pred(lambda c: c.data == tree_name)), token_name
+    )
 
 def get_identifiers(tree: ParseTree):
     return (token.value for token in tree.scan_values(lambda v: v.type == "IDENTIFIER"))
@@ -48,7 +53,7 @@ def build_interface_declaration(tree: ParseTree, context: Context, package_prefi
 
     extends = list(
         map(
-            lambda e: get_nested_token(e, "IDENTIFIER"),
+            lambda e: get_nested_token(e, "NAME"),
             tree.find_pred(lambda c: c.data == "class_type"),
         )
     )
@@ -59,7 +64,7 @@ def build_interface_declaration(tree: ParseTree, context: Context, package_prefi
     nested_context = Context(context, symbol)
     context.children.append(nested_context)
     build_environment(
-        next(tree.find_pred(lambda c: c.data == "interface_body")),
+        next(tree.find_data("interface_body")),
         nested_context,
     )
 
@@ -126,27 +131,25 @@ def parse_node(tree: ParseTree, context: Context):
         case "constructor_declaration":
             modifiers = list(map(lambda m: m.value, get_modifiers(tree.children)))
 
-            formal_params = next(tree.find_pred(lambda c: c.data == "formal_param_list"), None)
-            if formal_params is not None:
-                formal_param_types = list(
-                    map(
-                        lambda fp: next(
-                            map(
-                                lambda t: next(t.scan_values(lambda v: isinstance(v, Token))),
-                                fp.find_pred(lambda c: c.data == "type"),
-                            )
-                        ),
-                        formal_params.children,
-                    )
+            formal_params = next(tree.find_data("formal_param_list"), None)
+            formal_param_types = list(
+                map(
+                    lambda fp: next(
+                        map(lambda t: next(t.scan_values(lambda v: isinstance(v, Token))), fp.find_data("type"))
+                    ),
+                    formal_params.children,
                 )
-            else:
-                formal_param_types = []
+            ) if formal_params is not None else []
 
             symbol = ConstructorDecl(context, formal_param_types, modifiers)
             logging.debug("constructor_declaration %s %s", formal_param_types, modifiers)
             context.declare(symbol)
 
-            if (nested_tree := next(tree.find_pred(lambda c: c.data == "block"), None)) is not None:
+            #TODO: Declare parameter variables in nested context
+
+            if (
+                nested_tree := next(filter(lambda c: isinstance(c, Tree) and c.data == "block", tree.children), None)
+            ) is not None:
                 nested_context = Context(context, symbol)
                 context.children.append(nested_context)
                 build_environment(nested_tree, nested_context)
@@ -155,31 +158,20 @@ def parse_node(tree: ParseTree, context: Context):
             modifiers = list(map(lambda m: m.value, get_modifiers(tree.children)))
             method_name = get_nested_token(tree, "IDENTIFIER")
 
-            formal_params = next(tree.find_pred(lambda c: c.data == "formal_param_list"), None)
-            if formal_params is not None:
-                formal_param_types = list(
-                    map(
-                        lambda fp: next(
-                            map(
-                                lambda t: next(t.scan_values(lambda v: isinstance(v, Token))),
-                                fp.find_pred(lambda c: c.data == "type"),
-                            )
-                        ),
-                        formal_params.children,
-                    )
+            formal_params = next(tree.find_data("formal_param_list"), None)
+            formal_param_types = list(
+                map(
+                    lambda fp: next(
+                        map(lambda t: next(t.scan_values(lambda v: isinstance(v, Token))), fp.find_data("type"))
+                    ),
+                    formal_params.children,
                 )
-            else:
-                formal_param_types = []
+            ) if formal_params is not None else []
 
             return_type = (
                 "void"
                 if any(isinstance(x, Token) and x.type == "VOID_KW" for x in tree.children)
-                else next(
-                    filter(
-                        lambda c: isinstance(c, Tree) and c.data == "type",
-                        tree.children,
-                    )
-                ).children[0]
+                else next(filter(lambda c: isinstance(c, Tree) and c.data == "type", tree.children)).children[0]
             )
 
             symbol = MethodDecl(context, method_name, formal_param_types, modifiers, return_type)
@@ -192,9 +184,16 @@ def parse_node(tree: ParseTree, context: Context):
             )
             context.declare(symbol)
 
-            if (nested_tree := next(tree.find_pred(lambda c: c.data == "block"), None)) is not None:
+            #TODO: Declare parameter variables in nested context
+
+            if (nested_tree := next(tree.find_data("method_body"), None)) is not None:
                 nested_context = Context(context, symbol)
                 context.children.append(nested_context)
+                print(nested_tree)
+                print()
+                for t in tree.find_data("method_body"):
+                    print(t)
+                    print()
                 build_environment(nested_tree, nested_context)
 
         case "field_declaration":
