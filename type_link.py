80b6f11eca554a9f34d9cae040751d04a749fba9
Ruyi Li <ruyili2002@gmail.com>
2024-02-16 09:58:10 -0500

    type link: package name prefixes, single name collisions, duplicate resolution

--- type_link.py
+++ type_link.py
@@ -19,6 +19,16 @@ def resolve_package_name(qualified_name: str) -> str:
     return ".".join(qualified_name.split(".")[:-1])
 
 
+def resolve_prefixes(qualified_name: str) -> List[str]:
+    prefixes = []
+    identifiers = qualified_name.split(".")
+    curr_name = ""
+    for i in range(len(identifiers)):
+        curr_name = f"{curr_name}.{identifiers[i]}" if curr_name else identifiers[i]
+        prefixes.append(curr_name)
+    return prefixes
+
+
 class ImportDeclaration:
     def link_type(self, context: Context, type_decl: ClassInterfaceDecl):
         raise NotImplementedError
@@ -38,8 +48,8 @@ class SingleTypeImport(ImportDeclaration):
     def link_type(self, context: Context, type_decl: ClassInterfaceDecl):
         logging.debug(f"Single Type Link: {self.name}, {type_decl.name}")
 
-        # No single-type-import declaration clashes with the class or interface declared in the same file.
-        if self.simple_name == type_decl.name:
+        # No single-type-import declaration clashes with the class or interface declared in the same file, but a class can import itself.
+        if self.name != type_decl.name and self.simple_name == resolve_simple_name(type_decl.name):
             raise SemanticError(f"Type {type_decl.name} clashes with import declaration {self.name}")
 
         # No two single-type-import declarations clash with each other.
@@ -55,7 +65,7 @@ class SingleTypeImport(ImportDeclaration):
         # All type names must resolve to some class or interface declared in some file listed on the Joos command line.
         imported_type = context.resolve(f"{ClassInterfaceDecl.node_type}^{self.name}")
         if imported_type is None:
-            raise SemanticError(f"Import {self.name} does not resolve to any provided type")
+            raise SemanticError(f"Import {self.name} does not resolve to any existing type")
 
         type_decl.type_names[self.simple_name] = imported_type
 
@@ -70,10 +80,10 @@ class OnDemandImport(ImportDeclaration):
     def link_type(self, context: Context, type_decl: ClassInterfaceDecl):
         logging.debug(f"On Demand Type Link: {self}, {type_decl.name}")
 
-        # Every import-on-demand declaration must refer to a package declared in some file listed on the
-        # Joos command line. That is, the import-on-demand declaration must refer to a package whose name
-        # appears as the package declaration in some source file, or whose name is a prefix of the name
-        # appearing in some package declaration.
+        # Every import-on-demand declaration must refer to a package declared in some file listed on
+        # the Joos command line. That is, the import-on-demand declaration must refer to a package
+        # whose name appears as the package declaration in some source file, or whose name is a
+        # prefix of the name appearing in some package declaration.
         prefix = self.package + "."
         for package in context.packages.keys():
             if package == self.package or package.startswith(prefix):
@@ -92,22 +102,40 @@ def resolve_type(context: Context, type_name: str, type_decl: ClassInterfaceDecl
         # resolve fully qualified type name
         symbol = context.resolve(f"{ClassInterfaceDecl.node_type}^{type_name}")
         if symbol is None:
-            raise SemanticError(f"Full qualified type {type_name} does not resolve to any provided type")
+            raise SemanticError(f"Full qualified type {type_name} does not resolve to any existing type")
 
         type_decl.type_names[type_name] = symbol
 
     else:
         # resolve simple type name
 
-        # all single imports are already resolved, so look at on demand imports
+        # all simple imports are already resolved, so look at on demand imports
+        found_import = False
         for import_decl in type_decl.imports:
             if isinstance(import_decl, OnDemandImport):
                 symbol = context.resolve(f"{ClassInterfaceDecl.node_type}^{import_decl.package}.{type_name}")
                 if symbol is not None:
+                    if type_decl.type_names.get(type_name) is not None:
+                        raise SemanticError(
+                            f"Simple type {type_name} resolves to a type in the same environment as a type from an on demand import (conflicting resolutions: {type_decl.type_names[type_name].name}, {symbol.name})"
+                        )
                     type_decl.type_names[type_name] = symbol
-                    return
+                    found_import = True
 
-        raise SemanticError(f"Simple type {type_name} does not resolve to any provided type")
+        if not found_import:
+            raise SemanticError(f"Simple type {type_name} does not resolve to any existing type")
+
+
+def check_type_clashes(context: Context, type_name: str, type_decl: ClassInterfaceDecl):
+    is_qualified = "." in type_name
+    if is_qualified:
+        # When a fully qualified name resolves to a type, no strict prefix of the fully qualified
+        # name can resolve to a type in the same environment.
+        for prefix in resolve_prefixes(type_name)[:-1]:
+            if type_decl.type_names.get(prefix) is not None:
+                raise SemanticError(
+                    f"Prefix {prefix} of fully qualified type {type_name} resolves to a type in the same environment"
+                )
 
 
 def type_link(context: Context):
@@ -144,16 +172,15 @@ def type_link(context: Context):
             if symbol is None:
                 resolve_type(context, type_name, type_decl)
 
-        # When a fully qualified name resolves to a type, no strict prefix of the fully qualified name can resolve to a type in the same environment.
         for type_name in type_decl.type_names.keys():
-            if "." not in type_name:
-                continue
+            check_type_clashes(context, type_name, type_decl)
 
-            identifiers = type_name.split(".")
-            curr_name = ""
-            for i in range(len(identifiers) - 1):
-                curr_name = f"{curr_name}.{identifiers[i]}" if curr_name else identifiers[i]
-                if type_decl.type_names.get(curr_name) is not None:
-                    raise SemanticError(
-                        f"Prefix of fully qualified type {type_name} resolves to a type in the same environment"
-                    )
+    # No package names—including their prefixes—of declared packages, single-type-import
+    # declarations or import-on-demand declarations that are used may resolve to types,
+    # except for types in the default, unnamed package.
+    for package in context.packages.keys():
+        for prefix in resolve_prefixes(package)[1:]:
+            if context.resolve(f"{ClassInterfaceDecl.node_type}^{prefix}") is not None:
+                raise SemanticError(
+                    f"Prefix {prefix} of package {package} resolves to a type in the same environment"
+                )
