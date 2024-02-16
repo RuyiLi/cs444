5c4f19e7da15a3409f3bbffbb94e1d04923accbb
Ruyi Li <r397li@uwaterloo.ca>
2024-02-15 22:51:36 -0500

    import testcases

--- custom_testcases/import1.java
+++ custom_testcases/import1.java
@@ -1,6 +1,8 @@
 import foo.bar.baz;
 import java.util.Vector;
 import javax.swing.*;
-import foo.* ;import _foo.$bar_4.* ;
+import foo.* ;import _foo.b$ar_4.* ;
 import something    .something1.    something2.   something3       ;
                                     import single;
+
+public interface import1 {}
---
+++ custom_testcases/import2.java
@@ -0,0 +1,15 @@
+package foo.bar;
+
+import foo.bar.baz;
+// import foo.bar.baz;
+// import foo.baz;
+import foo.qux.*;
+import java.util.Vector;
+// import asdf.Vector;
+    import qux.ijfo___$a.baz.foo;
+
+// import fake.import2;;;;;;;;;;
+
+public class import2 {
+    public import2(){}
+}
