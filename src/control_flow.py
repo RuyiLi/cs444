7f181bed81ca4c24877f1516a1895637fee7383d
WillFlame <flameyanai@gmail.com>
2024-03-15 23:34:28 -0400

    Implement constant expressions

--- src/control_flow.py
+++ src/control_flow.py
@@ -4,8 +4,10 @@ from typing import Set, List, Tuple
 from lark import Token, Tree
 from context import Context, SemanticError, LocalVarDecl, FieldDecl, Symbol
 
+from context import LocalVarDecl, Symbol
 from helper import extract_name, get_child_tree, get_tree_token, is_static_context
-
+from joos_types import is_numeric_type
+from type_check import resolve_token
 
 class CFGNode:
     in_vars: Set[str]
@@ -34,7 +36,45 @@ class CFGNode:
         return self.pretty(set())
 
 
-def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode]]:
+def resolve_const_expr(expr: Tree | Token):
+    if isinstance(expr, Token):
+        expr_type = resolve_token(expr, None)
+
+        if is_numeric_type(expr_type):
+            return int(expr.value)
+        elif expr_type.name == "boolean":
+            return expr.value == "true"
+        else:
+            return expr.value
+
+    match expr.data:
+        case "mult_expr":
+            lhs, rhs = [resolve_const_expr(expr.children[i]) for i in [0, -1]]
+            return lhs * rhs
+
+        case "add_expr":
+            lhs, rhs = [resolve_const_expr(expr.children[i]) for i in [0, -1]]
+            return lhs + rhs
+
+        case "rel_expr" | "eq_expr":
+            lhs, rhs = [resolve_const_expr(expr.children[i]) for i in [0, -1]]
+            op = expr.children[1]
+            return eval(f"{lhs} {op} {rhs}")
+
+        case "or_expr" | "and_expr" | "eager_or_expr" | "eager_and_expr":
+            lhs, rhs = [resolve_const_expr(expr.children[i]) for i in [0, -1]]
+            match expr.children[1]:
+                case "||" | "|":
+                    op = "or"
+                case "&&" | "&":
+                    op = "and"
+            return eval(f"{lhs} {op} {rhs}")
+
+        case "expr":
+            return resolve_const_expr(expr.children[0])
+
+
+def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode], bool]:
     """
     Returns the CFGNode corresponding to the given tree.
     Mutates parent_node.
@@ -47,18 +87,21 @@ def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode]]:
         case "block":
             if len(tree.children) == 0:
                 empty_node = CFGNode("empty_st", set(), set(), [])
-                return (empty_node, [empty_node])
+                return (empty_node, [empty_node], False)
 
             child_nodes_terminals = [make_cfg(child, context) for child in tree.children]
 
             for i in range(0, len(child_nodes_terminals) - 1):
-                _, l_terminals = child_nodes_terminals[i]
-                r_node, _ = child_nodes_terminals[i + 1]
+                _, l_terminals, non_terminal = child_nodes_terminals[i]
+                r_node, _, _ = child_nodes_terminals[i + 1]
+
+                if non_terminal:
+                    raise SemanticError("unreachable code after non-terminal loop")
 
                 for terminal in l_terminals:
                     terminal.next_nodes.append(r_node)
 
-            return (child_nodes_terminals[0][0], child_nodes_terminals[-1][1])
+            return (child_nodes_terminals[0][0], child_nodes_terminals[-1][1], False)
 
         case "local_var_declaration":
             expr = next(tree.find_data("var_initializer")).children[0]
@@ -74,7 +117,7 @@ def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode]]:
                         raise SemanticError(f"Self-assignment to variable {var_name}")
 
             var_node = CFGNode(tree.data, defs_expr | {var_name}, uses_expr)
-            return (var_node, [var_node])
+            return (var_node, [var_node], False)
 
         case "if_st" | "if_st_no_short_if":
             _if_kw, cond, true_block = tree.children
@@ -82,7 +125,7 @@ def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode]]:
 
             # We need to get the if statement context
             if_node = CFGNode(tree.data, defs_cond, uses_cond)
-            true_node, true_terminals = make_cfg(true_block, context)
+            true_node, true_terminals, _ = make_cfg(true_block, context)
 
             if_terminal = CFGNode("empty_st", set(), set(), [])
             if_node.next_nodes = [true_node, if_terminal]
@@ -90,7 +133,7 @@ def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode]]:
             for terminal in true_terminals:
                 terminal.next_nodes.append(if_terminal)
 
-            return (if_node, [if_terminal])
+            return (if_node, [if_terminal], False)
 
         case "if_else_st" | "if_else_st_no_short_if":
             # the way that these currently work is that if/else nodes have three children
@@ -101,24 +144,35 @@ def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode]]:
 
             # We need to get the if statement context
             if_else_node = CFGNode(tree.data, defs_cond, uses_cond)
-            true_node, true_terminals = make_cfg(true_block, context)
-            false_node, false_terminals = make_cfg(false_block, context)
+            true_node, true_terminals, true_non_terminal = make_cfg(true_block, context)
+            false_node, false_terminals, false_non_terminal = make_cfg(false_block, context)
 
             if_else_node.next_nodes = [true_node, false_node]
-            return (if_else_node, true_terminals + false_terminals)
+            return (if_else_node, true_terminals + false_terminals, true_non_terminal and false_non_terminal)
 
         case "while_st" | "while_st_no_short_if":
             _while_kw, cond, loop_body = tree.children
             defs_cond, uses_cond = decompose_expression(cond, context)
 
             cond_node = CFGNode(tree.data, defs_cond, uses_cond)
-            true_node, true_terminals = make_cfg(loop_body, context)
+            true_node, true_terminals, _ = make_cfg(loop_body, context)
             cond_node.next_nodes = [true_node]
 
             for terminal in true_terminals:
                 terminal.next_nodes.append(cond_node)
 
-            return (cond_node, [cond_node])
+            non_terminal = False
+
+            # No uses, so constant expression
+            if len(uses_cond) == 0:
+                const_expr_result = resolve_const_expr(get_child_tree(tree, "expr"))
+                if const_expr_result is True:
+                    non_terminal = True
+
+                if const_expr_result is False:
+                    raise SemanticError("Statements found inside false while loop")
+
+            return (cond_node, [cond_node], non_terminal)
 
         case "for_st" | "for_st_no_short_if":
             for_init, for_cond, for_update = (
@@ -134,7 +188,18 @@ def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode]]:
             #       for_cond   # circular reference
             #   rest_of_program
 
-            loop_body_node, loop_body_terminals = make_cfg(loop_body, context)
+            loop_body_node, loop_body_terminals, _ = make_cfg(loop_body, context)
+
+            non_terminal = False
+
+            # No uses, so constant expression
+            if len(for_cond[1]) == 0:
+                const_expr_result = resolve_const_expr(get_child_tree(tree, "expr"))
+                if const_expr_result is True:
+                    non_terminal = True
+
+                if const_expr_result is False:
+                    raise SemanticError("Statements found inside false for loop")
 
             for_cond_node = CFGNode(tree.data + "_cond", for_cond[0], for_cond[1], [loop_body_node])
             for_init_node = CFGNode(tree.data + "_init", for_init[0], for_init[1], [for_cond_node])
@@ -143,27 +208,27 @@ def make_cfg(tree: Tree, context: Context) -> tuple[CFGNode, List[CFGNode]]:
             for terminal in loop_body_terminals:
                 terminal.next_nodes.append(for_update_node)
 
-            return (for_init_node, [for_cond_node])
+            return (for_init_node, [for_cond_node], non_terminal)
 
         case "expr_st":
             # assignment | method_invocation | class_instance_creation
             defs_expr, uses_expr = decompose_expression(tree.children[0], context)
             expr_node = CFGNode(tree.data, defs_expr, uses_expr)
-            return (expr_node, [expr_node])
+            return (expr_node, [expr_node], False)
 
         case "return_st":
             defs_expr, uses_expr = set(), set()
             if len(tree.children) > 1:
                 defs_expr, uses_expr = decompose_expression(tree.children[1], context)
             return_node = CFGNode(tree.data, defs_expr, uses_expr)
-            return (return_node, [return_node])
+            return (return_node, [return_node], False)
 
         case "statement" | "statement_no_short_if":
             return make_cfg(tree.children[0], context)
 
         case "empty_st":
             empty_node = CFGNode("empty_st", set(), set(), [])
-            return (empty_node, [empty_node])
+            return (empty_node, [empty_node], False)
 
         case _:
             raise Exception(f"! CFG for {tree.data} not implemented")
