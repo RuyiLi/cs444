164315474099931e89494186b0316e268ff5efa1
WillFlame <flameyanai@gmail.com>
2024-03-25 12:58:16 -0400

    Complete TIR nodes

--- src/tir.py
+++ src/tir.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import List
+from typing import List, Dict
 
 class IRNode:
     label: str
@@ -76,11 +76,10 @@ class IRMem(IRExpr):
         self.address = address
 
     def __repr__(self):
-        return "LABEL"
+        return "MEM"
 
     def visit_children(self, visitor):
         child_expr = visitor.visitor(self, self.address)
-
         return IRMem(child_expr) if child_expr != self.address else self
 
 
@@ -115,7 +114,7 @@ class IRName(IRExpr):
         return f"NAME({self.name})"
 
 
-class IRESeq(IRESeq):
+class IRESeq(IRExpr):
     stmt: IRStmt
     expr: IRExpr
 
@@ -132,5 +131,145 @@ class IRESeq(IRESeq):
 
         if child_stmt != self.stmt or child_expr != self.expr:
             return IRESeq(child_stmt, child_expr)
+
+        return self
+
+
+class IRMove(IRStmt):
+    target: IRExpr
+    source: IRExpr
+
+    def __init__(self, target, source):
+        self.target = target
+        self.source = source
+
+    def __repr__(self):
+        return "MOVE"
+
+    def visit_children(self, visitor):
+        child_target = visitor.visit(self, self.target)
+        child_source = visitor.visit(self, self.source)
+
+        if child_target != self.target or child_source != self.target:
+            return IRMove(child_target, child_source)
+
+        return self
+
+
+class IRExp(IRStmt):
+    expr: IRExpr
+
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __repr__(self):
+        return "EXP"
+
+    def visit_children(self, visitor):
+        child_expr = visitor.visit(self, self.expr)
+        return IRExp(child_expr) if child_expr != self.expr else self
+
+
+class IRSeq(IRStmt):
+    stmts: List[IRStmt]
+    replace_parent: bool
+
+    def __init__(self, stmts, replace_parent = False):
+        self.stmts = stmts
+        self.replace_parent = replace_parent
+
+    def visit_children(self, visitor):
+        child_stmts = [visitor.visit(self, child) for child in self.stmts]
+
+        if any(stmt != child_stmts[i] for i, stmt in enumerate(self.stmts)):
+            return IRSeq(child_stmts)
+
+        return self
+
+
+class IRJump(IRStmt):
+    target: IRExpr
+
+    def __init__(self, target):
+        self.target = target
+
+    def __repr__(self):
+        return "JUMP"
+
+    def visit_children(self, visitor):
+        child_target = visitor.visit(self, self.target)
+        return IRJump(child_target) if child_target != self.target else self
+
+
+class IRCJump(IRStmt):
+    cond: IRExpr
+    true_label: str
+    false_label: str | None
+
+    def __init__(self, cond, true_label, false_label = None):
+        self.cond = cond
+        self.true_label = true_label
+        self.false_label = false_label
+
+    def visit_children(self, visitor):
+        child_cond = visitor.visit(self.cond)
+        return IRCJump(child_cond, self.true_label, self.false_label) if child_cond != self.cond else self
+
+
+class IRLabel(IRStmt):
+    name: str
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"LABEL({self.name})"
+
+
+class IRReturn(IRStmt):
+    ret: IRExpr
+
+    def __init__(self, ret):
+        self.ret = ret
+
+    def __repr__(self):
+        return "RETURN"
+
+    def visit_children(self, visitor):
+        child_ret = visitor.visit(self, self.ret)
+        return IRReturn(child_ret) if child_ret != self.ret else self
+
+
+class IRFuncDecl(IRNode):
+    name: str
+    body: IRStmt
+    num_params: int
+
+    def __init__(self, name, body, num_params):
+        self.name = name
+        self.body = body
+        self.num_params = num_params
+
+    def visit_children(self, visitor):
+        child_body = visitor.visit(self, self.body)
+        return IRFuncDecl(self.name, child_body, self.num_params) if child_body != self.body else self
+
+
+class IRCompUnit(IRNode):
+    name: str
+    functions: Dict[str, IRFuncDecl]
+
+    def __init__(self, name, functions = {}):
+        self.name = name
+        self.functions = functions
+
+    def __repr__(self):
+        return "COMPUNIT"
+
+    def visit_children(self, visitor):
+        child_funcs = [visitor.visit(self, func) for _, func in self.functions]
+
+        if any(func != child_funcs[i] for i, func in enumerate(self.funcs)):
+            return IRCompUnit(self.name, child_funcs)
 
         return self
