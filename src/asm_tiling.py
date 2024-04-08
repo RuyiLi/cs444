58129feea805dc3ca7a2d969d7365a60a7f0a2d1
Chinemerem <chinemeremchigbo@Outlook.com>
2024-04-08 15:31:51 -0400

    Reformat asm_tiling

--- src/asm_tiling.py
+++ src/asm_tiling.py
@@ -3,282 +3,264 @@ from functools import reduce
 from typing import Dict, List, Tuple
 
 from tir import (
-	IRBinExpr,
-	IRCall,
-	IRCJump,
-	IRConst,
-	IRExpr,
-	IRFuncDecl,
-	IRJump,
-	IRLabel,
-	IRMem,
-	IRMove,
-	IRName,
-	IRReturn,
-	IRSeq,
-	IRStmt,
-	IRTemp,
+    IRBinExpr,
+    IRCall,
+    IRCJump,
+    IRConst,
+    IRExpr,
+    IRFuncDecl,
+    IRJump,
+    IRLabel,
+    IRMem,
+    IRMove,
+    IRName,
+    IRReturn,
+    IRSeq,
+    IRStmt,
+    IRTemp,
 )
 
 log = logging.getLogger(__name__)
 
+
 def tile_func(func: IRFuncDecl) -> List[str]:
-	asm = ["extern __malloc", "extern __exception", "extern __debexit", ""]
+    asm = ["extern __malloc", "extern __exception", "extern __debexit", ""]
 
-	if func.name == "test":
-		asm += [
-			"global _start",
-			"_start:"
-		]
-	else:
-		asm += [
-			f"_{func.name}:",
-			"push ebp"			# Save old base pointer
-		]
+    if func.name == "test":
+        asm += ["global _start", "_start:"]
+    else:
+        asm += [
+            f"_{func.name}:",
+            "push ebp",  # Save old base pointer
+        ]
 
-	asm += ["mov ebp, esp"]		# Update base pointer to start in this call frame
+    asm += ["mov ebp, esp"]  # Update base pointer to start in this call frame
 
-	# Parameters are behind the return address on the stack, so we make them -2
-	local_var_dict = dict([(param, -(i+2)) for i, param in enumerate(func.params)])
-	local_var_dict['%RET'] = -100
+    # Parameters are behind the return address on the stack, so we make them -2
+    local_var_dict = dict([(param, -(i + 2)) for i, param in enumerate(func.params)])
+    local_var_dict["%RET"] = -100
 
-	asm += tile_stmt(func.body, local_var_dict)
+    asm += tile_stmt(func.body, local_var_dict)
 
-	if func.name != "test":
-		return asm
+    if func.name != "test":
+        return asm
 
-	main_return = [
-		"mov ebx, eax",		# Save final return
-		"mov eax, 1",		# sys_exit
-		"int 0x80"
-	]
+    main_return = [
+        "mov ebx, eax",  # Save final return
+        "mov eax, 1",  # sys_exit
+        "int 0x80",
+    ]
 
-	# Replace "ret" with special returns in main function
-	return reduce(lambda a, c: a + ([c] if c != "ret" else main_return), asm, [])
+    # Replace "ret" with special returns in main function
+    return reduce(lambda a, c: a + ([c] if c != "ret" else main_return), asm, [])
 
 
 def fmt_bp(index: int):
-	if index is None:
-		raise Exception('Attempted to format with None index!')
+    if index is None:
+        raise Exception("Attempted to format with None index!")
 
-	if index == -100:
-		return "eax"
+    if index == -100:
+        return "eax"
 
-	if index < 0:
-		return f"[ebp+{-4*index}]"
+    if index < 0:
+        return f"[ebp+{-4*index}]"
 
-	index += 1
-	return f"[ebp-{4*index}]"
+    index += 1
+    return f"[ebp-{4*index}]"
 
 
-def process_expr(expr: IRExpr, local_var_dict: Dict[str, int], reg = "ecx") -> Tuple[str | int, List[str]]:
-	log.info(f"processing expr {expr}, {local_var_dict}")
+def process_expr(expr: IRExpr, local_var_dict: Dict[str, int], reg="ecx") -> Tuple[str | int, List[str]]:
+    log.info(f"processing expr {expr}, {local_var_dict}")
 
-	if hasattr(expr, "name"):
-		expr.name = expr.name.split(".")[0]
+    if hasattr(expr, "name"):
+        expr.name = expr.name.split(".")[0]
 
-	if isinstance(expr, IRTemp):
-		return (reg, [f"mov {reg}, {fmt_bp(local_var_dict.get(expr.name))}"])
+    if isinstance(expr, IRTemp):
+        return (reg, [f"mov {reg}, {fmt_bp(local_var_dict.get(expr.name))}"])
 
-	if isinstance(expr, IRConst):
-		return (expr.value, [])
+    if isinstance(expr, IRConst):
+        return (expr.value, [])
 
-	if isinstance(expr, IRBinExpr):
-		return (reg, tile_expr(expr, reg, local_var_dict))
+    if isinstance(expr, IRBinExpr):
+        return (reg, tile_expr(expr, reg, local_var_dict))
 
-	if isinstance(expr, IRMem):
-		return "temp_remove_this", []
+    if isinstance(expr, IRMem):
+        return "temp_remove_this", []
 
-	raise Exception(f"unable to process expr {expr}")
+    raise Exception(f"unable to process expr {expr}")
 
 
 def tile_stmt(stmt: IRStmt, local_var_dict: Dict[str, int]) -> List[str]:
-	log.info(f"tiling stmt {stmt}")
+    log.info(f"tiling stmt {stmt}")
 
-	match stmt:
-		case IRCall(target=t, args=args):
-			if t.name == "__malloc":
-				r, r_asm = process_expr(args[0], local_var_dict)
+    match stmt:
+        case IRCall(target=t, args=args):
+            if t.name == "__malloc":
+                r, r_asm = process_expr(args[0], local_var_dict)
 
-				# malloc() allocates eax bytes and returns address in eax
-				return r_asm + [f"mov eax, {r}", "call __malloc"]
+                # malloc() allocates eax bytes and returns address in eax
+                return r_asm + [f"mov eax, {r}", "call __malloc"]
 
-			if t.name == "__exception":
-				t.name = "_exception"
+            if t.name == "__exception":
+                t.name = "_exception"
 
-			asm = []
+            asm = []
 
-			for arg in args:
-				r, r_asm = process_expr(arg, local_var_dict)
-				asm += r_asm + [f"push {r}"]
+            for arg in args:
+                r, r_asm = process_expr(arg, local_var_dict)
+                asm += r_asm + [f"push {r}"]
 
-			asm += [
-				f"call _{t.name.split('.')[-1]}",	# FIX: Hack to try and call local function
-				f"add esp, {len(args)*4}"	# pop off arguments
-			]
-			return asm
+            asm += [
+                f"call _{t.name.split('.')[-1]}",  # FIX: Hack to try and call local function
+                f"add esp, {len(args)*4}",  # pop off arguments
+            ]
+            return asm
 
-		case IRCJump(cond=c, true_label=t):
-			match c:
-				case IRConst(value=v):
-					if v == 0:	# never jump
-						return []
-					if v == 1:	# always jump
-						return [f"jmp {t.name}"]
+        case IRCJump(cond=c, true_label=t):
+            match c:
+                case IRConst(value=v):
+                    if v == 0:  # never jump
+                        return []
+                    if v == 1:  # always jump
+                        return [f"jmp {t.name}"]
 
-					raise Exception(f"CJump with constant value {v} not 0 or 1!!")
+                    raise Exception(f"CJump with constant value {v} not 0 or 1!!")
 
-				case IRTemp(name=n):
-					if (loc := local_var_dict.get(n, None)) is not None:
-						return [
-							f"mov eax, {fmt_bp(loc)}",
-							"cmp eax, 1",
-							f"je {t.name}"
-						]
-					raise Exception(f"CJump with unbound variable {n}")
+                case IRTemp(name=n):
+                    if (loc := local_var_dict.get(n, None)) is not None:
+                        return [f"mov eax, {fmt_bp(loc)}", "cmp eax, 1", f"je {t.name}"]
+                    raise Exception(f"CJump with unbound variable {n}")
 
-				case IRBinExpr(op_type=o, left=l, right=r):
-					asm = []
-					assert isinstance(l, IRTemp)
+                case IRBinExpr(op_type=o, left=l, right=r):
+                    asm = []
+                    assert isinstance(l, IRTemp)
 
-					left = local_var_dict.get(l.name)
+                    left = local_var_dict.get(l.name)
 
-					right, r_asm = process_expr(r, local_var_dict)
-					asm += r_asm
+                    right, r_asm = process_expr(r, local_var_dict)
+                    asm += r_asm
 
-					match o:
-						case "EQ":
-							return asm + [
-								f"mov edx, {fmt_bp(left)}",
-								f"cmp edx, {right}",
-								f"je {t.name}"
-							]
-						case "LT":
-							return asm + [
-								f"mov edx, {fmt_bp(left)}",
-								f"cmp edx, {right}",
-								f"jl {t.name}"
-							]
-						case "NOT_EQ":
-							return asm + [
-								f"mov edx, {fmt_bp(left)}",
-								f"cmp edx, {right}",
-								f"jne {t.name}"
-							]
-						case "LOGICAL_AND":
-							return asm + [
-								f"mov edx, {fmt_bp(left)}",
-								f"and edx, {right}",
-								"cmp edx, 1",
-								f"je {t.name}"
-							]
-						case x:
-							raise Exception(f"CJump with unimplemented cond IRBinExpr with optype {o}")
+                    match o:
+                        case "EQ":
+                            return asm + [f"mov edx, {fmt_bp(left)}", f"cmp edx, {right}", f"je {t.name}"]
+                        case "LT":
+                            return asm + [f"mov edx, {fmt_bp(left)}", f"cmp edx, {right}", f"jl {t.name}"]
+                        case "NOT_EQ":
+                            return asm + [f"mov edx, {fmt_bp(left)}", f"cmp edx, {right}", f"jne {t.name}"]
+                        case "LOGICAL_AND":
+                            return asm + [
+                                f"mov edx, {fmt_bp(left)}",
+                                f"and edx, {right}",
+                                "cmp edx, 1",
+                                f"je {t.name}",
+                            ]
+                        case x:
+                            raise Exception(f"CJump with unimplemented cond IRBinExpr with optype {o}")
 
-				case x:
-					raise Exception(f"CJump with unimplemented condition {c}")
+                case x:
+                    raise Exception(f"CJump with unimplemented condition {c}")
 
-		case IRJump(target=t):
-			assert isinstance(t, IRName)
-			return [f"jmp {t.name}"]
+        case IRJump(target=t):
+            assert isinstance(t, IRName)
+            return [f"jmp {t.name}"]
 
-		case IRLabel(name=n):
-			return [f"{n}:"]
+        case IRLabel(name=n):
+            return [f"{n}:"]
 
-		case IRMove(target=t, source=s):
-			match t:
-				case IRTemp(name=n):
-					if (loc := local_var_dict.get(n, None)) is not None:
-						log.info(f"VARIABLE {n} EXISTS AT {loc}")
-						r, r_asm = process_expr(s, local_var_dict)
-						return r_asm + [f"mov {fmt_bp(loc)}, {r}"]
+        case IRMove(target=t, source=s):
+            match t:
+                case IRTemp(name=n):
+                    if (loc := local_var_dict.get(n, None)) is not None:
+                        log.info(f"VARIABLE {n} EXISTS AT {loc}")
+                        r, r_asm = process_expr(s, local_var_dict)
+                        return r_asm + [f"mov {fmt_bp(loc)}, {r}"]
 
-					local_var_dict[n] = len([v for v in local_var_dict.values() if v >= 0])
-					log.info(f"CREATING NEW VAR {n} AT LOC {local_var_dict[n]}")
-					r, r_asm = process_expr(s, local_var_dict)
-					return r_asm + [f"push {r}"]
+                    local_var_dict[n] = len([v for v in local_var_dict.values() if v >= 0])
+                    log.info(f"CREATING NEW VAR {n} AT LOC {local_var_dict[n]}")
+                    r, r_asm = process_expr(s, local_var_dict)
+                    return r_asm + [f"push {r}"]
 
-				case IRMem(address=a):
-					assert isinstance(a, IRTemp)
-					l, l_asm = process_expr(a, local_var_dict, "eax")
+                case IRMem(address=a):
+                    assert isinstance(a, IRTemp)
+                    l, l_asm = process_expr(a, local_var_dict, "eax")
 
-					if isinstance(s, IRConst):
-						r, r_asm = ("ecx", [f"mov ecx, {s.value}"])
-					else:
-						r, r_asm = process_expr(s, local_var_dict, "ecx")
+                    if isinstance(s, IRConst):
+                        r, r_asm = ("ecx", [f"mov ecx, {s.value}"])
+                    else:
+                        r, r_asm = process_expr(s, local_var_dict, "ecx")
 
-					return l_asm + r_asm + [f"mov [{l}], {r}"]
+                    return l_asm + r_asm + [f"mov [{l}], {r}"]
 
-		case IRReturn(ret=ret):
-			if stmt.ret is None:
-				return []
+        case IRReturn(ret=ret):
+            if stmt.ret is None:
+                return []
 
-			# Return always uses eax register
-			stmts = tile_expr(stmt.ret, "eax", local_var_dict)
-			return stmts + ["mov esp, ebp", "pop ebp", "ret"]
+            # Return always uses eax register
+            stmts = tile_expr(stmt.ret, "eax", local_var_dict)
+            return stmts + ["mov esp, ebp", "pop ebp", "ret"]
 
-		case IRSeq(stmts=ss):
-			asm = []
-			for s in ss:
-				asm += tile_stmt(s, local_var_dict)
-			return asm
+        case IRSeq(stmts=ss):
+            asm = []
+            for s in ss:
+                asm += tile_stmt(s, local_var_dict)
+            return asm
 
-		case x:
-			raise Exception(f"Unknown tiling for statement {x}")
+        case x:
+            raise Exception(f"Unknown tiling for statement {x}")
 
-	return []
+    return []
 
 
 def tile_expr(expr: IRExpr, output_reg: str, local_var_dict: Dict[str, int]) -> List[str]:
-	asm = []
-	hold = ""
+    asm = []
+    hold = ""
 
-	log.info(f"tiling expr {expr}")
+    log.info(f"tiling expr {expr}")
 
-	match expr:
-		case IRConst(value=v):
-			hold = v
+    match expr:
+        case IRConst(value=v):
+            hold = v
 
-		case IRBinExpr(op_type=o, left=l, right=r):
-			assert isinstance(l, IRTemp)
+        case IRBinExpr(op_type=o, left=l, right=r):
+            assert isinstance(l, IRTemp)
 
-			left = local_var_dict.get(l.name)
+            left = local_var_dict.get(l.name)
 
-			right, r_asm = process_expr(r, local_var_dict)
-			asm += r_asm
+            right, r_asm = process_expr(r, local_var_dict)
+            asm += r_asm
 
-			asm.append(f"mov eax, {fmt_bp(left)}")
+            asm.append(f"mov eax, {fmt_bp(left)}")
 
-			if o == "MUL":
-				asm.append(f"imul eax, {right}")
+            if o == "MUL":
+                asm.append(f"imul eax, {right}")
 
-			elif o == "DIV":
-				if isinstance(r, IRConst):
-					asm.append(f"mov ebx, {right}")
-					right = "ebx"
+            elif o == "DIV":
+                if isinstance(r, IRConst):
+                    asm.append(f"mov ebx, {right}")
+                    right = "ebx"
 
-				asm += [
-					"xor edx, edx",		# Clear out edx for division
-					f"div {right}"		# Quotient stored in eax, remainder stored in edx
-				]
+                asm += [
+                    "xor edx, edx",  # Clear out edx for division
+                    f"div {right}",  # Quotient stored in eax, remainder stored in edx
+                ]
 
-			else:
-				asm.append(f"{o.lower()} eax, {right}")
+            else:
+                asm.append(f"{o.lower()} eax, {right}")
 
-			hold = "eax"
+            hold = "eax"
 
-		case IRTemp(name=n):
-			if (loc := local_var_dict.get(n, None)) is not None:
-				hold = fmt_bp(loc)
-			else:
-				log.info(f"{local_var_dict.get(n, None)}")
-				log.info(f"{local_var_dict}")
-				raise Exception(f"couldn't find local var {n} in dict!")
-		case x:
-			log.info(f"unknown expr type {x}")
+        case IRTemp(name=n):
+            if (loc := local_var_dict.get(n, None)) is not None:
+                hold = fmt_bp(loc)
+            else:
+                log.info(f"{local_var_dict.get(n, None)}")
+                log.info(f"{local_var_dict}")
+                raise Exception(f"couldn't find local var {n} in dict!")
+        case x:
+            log.info(f"unknown expr type {x}")
 
-	if output_reg != hold:
-		asm += [f"mov {output_reg}, {hold}"]
+    if output_reg != hold:
+        asm += [f"mov {output_reg}, {hold}"]
 
-	log.info(f"{asm}")
-	return asm
+    log.info(f"{asm}")
+    return asm
